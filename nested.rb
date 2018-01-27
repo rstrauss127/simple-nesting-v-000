e3ab6b00c8ca2eb0125b36fa5c33390cd5bfd537
Rebecca Strauss <rstrauss127@gmail.com>
2018-01-27 02:09:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,6 +60,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 dennis = programmer_hash[:dennis_ritchie]
+		 hash = dennis[:languages].to_string
 end
 
 def adding_matz
