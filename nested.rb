256854fd98f57d1430e60600e02ac3460fdabb15
Rebecca Strauss <rstrauss127@gmail.com>
2018-01-27 02:25:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -67,12 +67,6 @@ return hash[0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -88,6 +82,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
+		 
+		 programmer_hash.unshift(:yukihiro_matsumoto => {:known_for => "Ruby"}, :languages => ["LISP", "C"])
+		 # add the following information to the top level of programmer_hash
+		 # :yukihiro_matsumoto => {
+		 #   :known_for => "Ruby",
+		 #    :languages => ["LISP", "C"]
+		 # }
 
 
 end
